71f95d7e965343dae5f5f115048f94e406e4e703
Karol Jędrzejczyk <karaluh@karaluh.pl>
2020-04-17 20:19:39 +0200

    Naming cleanup.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScrobbleBrainz")]
+[assembly: AssemblyTitle("ScrobblerBrainz")]
 [assembly: AssemblyDescription("A scrobbler for ListenBrainz service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
