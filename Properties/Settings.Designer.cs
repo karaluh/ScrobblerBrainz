ce236a5622a44888b3ab049b142c8814e8619e38
karaluh <karaluh@karaluh.pl>
2021-08-22 19:13:09 +0200

    Fix default user token value

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MusicBeePlugin.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string userToken {
             get {
                 return ((string)(this["userToken"]));
